a1c7de7c691c39a0a71ef87e5339eb0c712db86d
Jack Lovett <jack.l.lovett@gmail.com>
2024-07-17 11:17:53 +0100

    Revert tested content changes

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,14 +4,14 @@ export const Home = () => {
     return (
         <div className="about-container">
             <div className="about-content">
-                {/* <h1>Jack Lovett</h1> */}
-                {/* <p>
+                <h1>Jack Lovett</h1>
+                <p>
                     Hi! I'm an experienced developer who's worked with a variety
                     of tech stacks, mostly focusing on web development. I am
                     passionate about creating intuitive and engaging UI/UX
                     designs that enhance user experiences and make people's
                     lives easier and more enjoyable.
-                </p> */}
+                </p>
                 <p>
                     I'm looking for new opportunities to work on innovative
                     projects with a great team. If you're interested in
