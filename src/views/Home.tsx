351112d991da2dc5816d1eb316aa7be369685971
Jack Lovett <jack.l.lovett@gmail.com>
2024-09-03 09:25:09 +0100

    Fix font sizing

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,10 +7,10 @@ export const Home = () => {
             <div className="about-content">
                 <RetroComputer animationText="Hello World!" />
                 <h1>Jack Lovett</h1>
-                <p className="highlight">
+                <h2 className="highlight">
                     Full-stack Developer <span className="divider">|</span> UX
                     Enthusiast
-                </p>
+                </h2>
                 <p>
                     I’m passionate about creating intuitive and engaging digital
                     experiences across all platforms. Let's create user-focused
